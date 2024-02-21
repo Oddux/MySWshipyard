90687aef12bdcc44f01e9e6f59247819205a72ec
Oddux <141483016+Oddux@users.noreply.github.com>
2024-02-21 02:45:30 -0700

    more graphql functionality

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-import '.styles/app.css';
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+
 const app = express();
+
+const ships = [];
+
 
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Ship {
+            _id: ID!
+            model: String!
+            manufacturer: String!
+            length: String!
+            max_atmosphering_speed: String!
+            crew: String!
+            passengers: String!
+            cargo_capacity: String!
+            consumables: String!
+            hyperdrive_rating: String!
+            MGLT: String!
+            starship_class: String!
+        }
+
+        input ShipInput {
+            model: String!
+            manufacturer: String!
+            length: String!
+            max_atmosphering_speed: String!
+            crew: String!
+            passengers: String!
+            cargo_capacity: String!
+            consumables: String!
+            hyperdrive_rating: String!
+            MGLT: String!
+            starship_class: String!
+        }
+
         type RootQuery {
-            ships: [String!]!
-            
+            ships: [Ship!]!            
         }
 
         type RootMutation {
-            createShip(shipInput: String): String
+            createShip(shipInput: ShipInput): Ship
         }
 
         schema {
@@ -26,11 +56,24 @@ app.use('/graphql', graphqlHttp({
     rootValue: 
     {
         ships: () => {
-            return ['Millennium Falcon', 'X-Wing', 'TIE Fighter'];
+            return [ships];
         },
         createShip: (args) => {
-            const shipName = args.shipInput;
-            return shipName;
+            const ship = {
+                _id: Math.random().toString(),
+                model: args.model,
+                manufacturer: args.manufacturer,
+                length: args.length,
+                max_atmosphering_speed: args.max_atmosphering_speed,
+                crew: args.crew,
+                passengers: args.passengers,
+                cargo_capacity: args.cargo_capacity,
+                consumables: args.consumables,
+                hyperdrive_rating: args.hyperdrive_rating,
+                MGLT: args.MGLT,
+                starship_class: args.starship_class
+            };
+            ships.push(ship);
         }
     },
     graphiql: true
